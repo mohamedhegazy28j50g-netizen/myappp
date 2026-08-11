04b3a4e7ae4c2126ce02027e91835c1bee2d50ff
mohamedhegazy28j50g-netizen <mohamed.hegazy.28j50g@gmail.com>
2026-08-11 15:22:38 +0300

    ...

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express          = require("express");
 const bcrypt           = require("bcrypt");
 const crypto           = require("crypto");
-const User             = require("../models/User");
+const User             = require("../models/user");
 const VerificationCode = require("../models/VerificationCode");
 
 
